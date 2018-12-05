63cfec5d22573634988bf715a80f3073afe33461
isorresso <43363976+isorresso@users.noreply.github.com>
2018-12-05 16:03:15 -0500

    added bootstrap

--- js/final-project.js
+++ js/final-project.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
 $('#schurfacts').hide();
   $('#schur').click(function() {
     $('#schurfacts').slideToggle();
@@ -30,4 +29,24 @@ $('#parksfacts').hide();
       $('#dev').click(function() {
         $('#masterfacts').slideToggle();
       });
+
 });
+
+/*
+$(document).ready(() => {
+
+  $(".facts").hide();
+
+    $(".pix").on("click", event => {
+        $(event.currentTarget)
+          .closest("section")
+          .siblings()
+          .find(".facts")
+          .slideUp();
+
+          $(event.currentTarget)
+                .parent()
+                .next()
+                .slideToggle();
+      });
+*/
