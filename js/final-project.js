9073d7337c1b3a0dfbfb064a8228284ac04d6aa7
isorresso <43363976+isorresso@users.noreply.github.com>
2018-12-05 01:42:36 -0500

    Initial commit

---
+++ js/final-project.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+
+$('#schurfacts').hide();
+  $('#schur').click(function() {
+    $('#schurfacts').slideToggle();
+  });
+
+$('#parksfacts').hide();
+  $('#leslie').click(function() {
+    $('#parksfacts').slideToggle();
+  });
+
+  $('#officefacts').hide();
+    $('#michael').click(function() {
+      $('#officefacts').slideToggle();
+    });
+
+    $('#brooklynfacts').hide();
+      $('#jake').click(function() {
+        $('#brooklynfacts').slideToggle();
+      });
+
+    $('#goodplacefacts').hide();
+      $('#eleanor').click(function() {
+        $('#goodplacefacts').slideToggle();
+      });
+
+    $('#masterfacts').hide();
+      $('#dev').click(function() {
+        $('#masterfacts').slideToggle();
+      });
+});
