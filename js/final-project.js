2cf0481537d938c94e9b1a6a8352490a365f45f6
isorresso <43363976+isorresso@users.noreply.github.com>
2018-12-07 14:02:51 -0500

    fixed collapse buttons

--- js/final-project.js
+++ js/final-project.js
@@ -1,52 +1,109 @@
-$(document).ready(function() {
+// Quiz result options in a separate object for flexibility
+var resultOptions = [
+    {   title: 'You should be a Software Dev!',
+        desc: '<p>These are tools for making other tools! Software is huge and slick and complicated. Hope youre prepared to keep updating this for months or years. The work is very focused and youll spend a lot of time staring at your computer screen.'},
+    {   title: 'You should be a Backend Dev!',
+        desc: '<p>Backend Development involves setting up databases and figuring out how to best manipulate your data to create websites and applications.</p>'},
+    {   title: 'You should be a Frontend Dev!',
+        desc: '<p>Frontend Developers work with Javascript, HTML, and CSS to make the wireframe created by the backend developers palettable to human eyes and hands. Its similar to UX/UI.</p>'},
+    {   title: 'You Should Be a UX Designer!',
+        desc: '<p>Developers dont like making interfaces. You do. Take their (probably ugly) output and make something that users will not only use, but will WANT to use.</p>'},
+    {   title: 'You should be a PM!',
+        desc: '<p>Theres not a lot of actual coding, so youll need to know more theory than skills. For large projects, though, youre incredibly important because youll be organizing the code monkeys (developers) and ensuring on-time delivery. You focus on the big picture but keep your eye on the deadlines. Youre good at nuturing relationships with clients.</p>'}
+];
 
-$('#schurfacts').hide();
-  $('#schur').click(function() {
-    $('#schurfacts').slideToggle();
-  });
+// global variables
+var quizSteps = $('#quizzie .quiz-step'),
+    totalScore = 0;
 
-$('#parksfacts').hide();
-  $('#leslie').click(function() {
-    $('#parksfacts').slideToggle();
-  });
-
-  $('#officefacts').hide();
-    $('#michael').click(function() {
-      $('#officefacts').slideToggle();
+// for each step in the quiz, add the selected answer value to the total score
+// if an answer has already been selected, subtract the previous value and update total score with the new selected answer value
+// toggle a visual active state to show which option has been selected
+quizSteps.each(function () {
+    var currentStep = $(this),
+        ansOpts = currentStep.children('.quiz-answer');
+    // for each option per step, add a click listener
+    // apply active class and calculate the total score
+    ansOpts.each(function () {
+        var eachOpt = $(this);
+        eachOpt[0].addEventListener('click', check, false);
+        function check() {
+            var $this = $(this),
+                value = $this.attr('data-quizIndex'),
+                answerScore = parseInt(value);
+            // check to see if an answer was previously selected
+            if (currentStep.children('.active').length > 0) {
+                var wasActive = currentStep.children('.active'),
+                    oldScoreValue = wasActive.attr('data-quizIndex'),
+                    oldScore = parseInt(oldScoreValue);
+                // handle visual active state
+                currentStep.children('.active').removeClass('active');
+                $this.addClass('active');
+                // handle the score calculation
+                totalScore -= oldScoreValue;
+                totalScore += answerScore;
+                calcResults(totalScore);
+            } else {
+                // handle visual active state
+                $this.addClass('active');
+                // handle score calculation
+                totalScore += answerScore;
+                calcResults(totalScore);
+                // handle current step
+                updateStep(currentStep);
+            }
+        }
     });
-
-    $('#brooklynfacts').hide();
-      $('#jake').click(function() {
-        $('#brooklynfacts').slideToggle();
-      });
-
-    $('#goodplacefacts').hide();
-      $('#eleanor').click(function() {
-        $('#goodplacefacts').slideToggle();
-      });
-
-    $('#masterfacts').hide();
-      $('#dev').click(function() {
-        $('#masterfacts').slideToggle();
-      });
-
 });
 
-/*
-$(document).ready(() => {
+// show current step/hide other steps
+function updateStep(currentStep) {
+    if(currentStep.hasClass('current')){
+       currentStep.removeClass('current');
+       currentStep.next().addClass('current');
+    }
+}
 
-  $(".facts").hide();
+// display scoring results
+function calcResults(totalScore) {
+    // only update the results div if all questions have been answered
+    if (quizSteps.find('.active').length == quizSteps.length){
+        var resultsTitle = $('#results h1'),
+            resultsDesc = $('#results .desc');
 
-    $(".pix").on("click", event => {
-        $(event.currentTarget)
-          .closest("section")
-          .siblings()
-          .find(".facts")
-          .slideUp();
-
-          $(event.currentTarget)
-                .parent()
-                .next()
-                .slideToggle();
-      });
-*/
+        // calc lowest possible score
+        var lowestScoreArray = $('#quizzie .low-value').map(function() {
+            return $(this).attr('data-quizIndex');
+        });
+        var minScore = 0;
+        for (var i = 0; i < lowestScoreArray.length; i++) {
+            minScore += lowestScoreArray[i] << 0;
+        }
+        // calculate highest possible score
+        var highestScoreArray = $('#quizzie .high-value').map(function() {
+            return $(this).attr('data-quizIndex');
+        });
+        var maxScore = 0;
+        for (var i = 0; i < highestScoreArray.length; i++) {
+            maxScore += highestScoreArray[i] << 0;
+        }
+        // calc range, number of possible results, and intervals between results
+        var range = maxScore - minScore,
+            numResults = resultOptions.length,
+            interval = range / (numResults - 1),
+            increment = '',
+            n = 0; //increment index
+        // incrementally increase the possible score, starting at the minScore, until totalScore falls into range. then match that increment index (number of times it took to get totalScore into range) and return the corresponding index results from resultOptions object
+        while (n < numResults) {
+            increment = minScore + (interval * n);
+            if (totalScore <= increment) {
+                // populate results
+                resultsTitle.replaceWith("<h1>" + resultOptions[n].title + "</h1>");
+                resultsDesc.replaceWith("<p class='desc'>" + resultOptions[n].desc + "</p>");
+                return;
+            } else {
+                n++;
+            }
+        }
+    }
+}
